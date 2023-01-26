8768e1bbfb5abe8cf99e2f77fca36d2109f6b15c
Ilario FAVERO <ilario.favero@eurecom.fr>
2023-01-26 11:34:24 +0100

    realingment with master

--- src/agent/msg_handler_agent.c
+++ src/agent/msg_handler_agent.c
@@ -153,6 +153,8 @@ e2ap_msg_t e2ap_handle_subscription_request_agent(e2_agent_t* ag, const e2ap_msg
   ev.sm = sm;
   bi_map_insert(&ag->ind_event, &fd_timer, sizeof(fd_timer), &ev, sizeof(ev));
 
+  printf("[E2-AGENT]: RIC_SUBSCRIPTION_REQUEST rx\n");
+
   uint8_t const ric_act_id = sr->action[0].id;
   e2ap_msg_t ans = {.type = RIC_SUBSCRIPTION_RESPONSE, 
                     .u_msgs.ric_sub_resp = generate_subscription_response(&sr->ric_id, ric_act_id) };

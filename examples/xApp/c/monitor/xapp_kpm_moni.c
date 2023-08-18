067e49719411c955e65c031b420777332eb5b187
Teodora <teodora.vladic@openairinterface.org>
2023-08-18 16:35:32 +0200

    Updated KPM v3.00 xApp

--- examples/xApp/c/monitor/xapp_kpm_moni.c
+++ examples/xApp/c/monitor/xapp_kpm_moni.c
@@ -22,61 +22,241 @@
 #include "../../../../src/xApp/e42_xapp_api.h"
 #include "../../../../src/util/alg_ds/alg/defer.h"
 #include "../../../../src/util/time_now_us.h"
-#include "../../../../src/sm/kpm_sm_v03.00/kpm_sm_id.h"
+#include "../../../../src/util/alg_ds/ds/lock_guard/lock_guard.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
 #include <signal.h>
+#include <pthread.h>
+
+
+static
+pthread_mutex_t mtx;
 
 static
 void sm_cb_kpm(sm_ag_if_rd_t const* rd)
 {
   assert(rd != NULL);
-  assert(rd->type ==INDICATION_MSG_AGENT_IF_ANS_V0);
-  assert(rd->ind.type == KPM_STATS_V0);
+  assert(rd->type == INDICATION_MSG_AGENT_IF_ANS_V0);
+  assert(rd->ind.type == KPM_STATS_V3_0);
 
-  const kpm_ind_hdr_t* hdr = &rd->kpm_stats.hdr;
-  printf("received KPM indication at %d (sender '%s', type '%s', vendor '%s')\n",
-         hdr->collectStartTime, (char *)(hdr->sender_name.buf), (char *)(hdr->sender_type.buf), (char *)(hdr->vendor_name.buf));
+  // Reading Indication Message Format 3
+  kpm_ind_data_t const* ind = &rd->ind.kpm.ind;
+  kpm_ric_ind_hdr_format_1_t const* hdr_frm_1 = &ind->hdr.kpm_ric_ind_hdr_format_1;
+  kpm_ind_msg_format_3_t const* msg_frm_3 = &ind->msg.frm_3;
+  
 
-  const kpm_ind_msg_t* msg = &rd->kpm_stats.msg;
-  assert(msg->MeasInfo_len == msg->MeasData_len);
-  for (size_t i = 0; i < msg->MeasInfo_len; ++i) {
-    MeasInfo_t* mi = &msg->MeasInfo[i];
-    assert(mi->meas_type == KPM_V2_MEASUREMENT_TYPE_NAME);
-    printf("%s ", (char *)(mi->meas_name.buf));
+  int64_t now = time_now_us();
 
-    if (msg->MeasData[i].measRecord_len == 2) {
-      const MeasRecord_t* mdi = msg->MeasData[i].measRecord;
-      const long ctx = mdi[0].int_val;
-      const long crx = mdi[1].int_val;
-      if (i % 2 == 0){
-        static long tx[10] = {0};
-        static long rx[10] = {0};
-        const float thrtx = (float) (ctx - tx[i]) * 8 / 1e6;
-        tx[i] = ctx;
-        const float thrrx = (float) (crx - rx[i]) * 8 / 1e6;
-        rx[i] = crx;
-        printf(" TX %7.3f RX %7.3f (Mbps)\n                         ", thrtx, thrrx);
+  {
+    lock_guard(&mtx);
+
+    static int counter = 1;
+    printf("\n%7d KPM ind_msg latency = %ld [μs]\n", counter, now - hdr_frm_1->collectStartTime);  // xApp <-> E2 Node
+
+    // Reported list of measurements per UE
+    for (size_t i = 0; i < msg_frm_3->ue_meas_report_lst_len; i++)
+    {
+      switch (msg_frm_3->meas_report_per_ue[i].ue_meas_report_lst.type)
+      {
+      case GNB_UE_ID_E2SM:
+        printf("UE ID type = gNB, amf_ue_ngap_id = %lu\n", msg_frm_3->meas_report_per_ue[i].ue_meas_report_lst.gnb.amf_ue_ngap_id);
+        break;
+      
+      default:
+        assert(false && "UE ID type not yet implemented");
       }
-      printf(" TX %7ld RX %7ld", ctx, crx);
-    } else {
-      for (size_t j = 0; j < msg->MeasData[i].measRecord_len; ++j) {
-        const MeasRecord_t* mdi = &msg->MeasData[i].measRecord[j];
-        printf(" [%ld] %7ld", j, mdi->int_val);
+
+      kpm_ind_msg_format_1_t const* msg_frm_1 = &msg_frm_3->meas_report_per_ue[i].ind_msg_format_1;
+
+      // UE Measurements per granularity period
+      for (size_t j = 0; j<msg_frm_1->meas_data_lst_len; j++)
+      {
+        for (size_t z = 0; z<msg_frm_1->meas_data_lst[j].meas_record_len; z++)
+        {
+          if (msg_frm_1->meas_info_lst_len > 0)
+          {
+            switch (msg_frm_1->meas_info_lst[z].meas_type.type)
+            {
+            case NAME_MEAS_TYPE:
+            {
+              // Get the Measurement Name
+              char meas_info_name_str[msg_frm_1->meas_info_lst[z].meas_type.name.len + 1];
+              memcpy(meas_info_name_str, msg_frm_1->meas_info_lst[z].meas_type.name.buf, msg_frm_1->meas_info_lst[z].meas_type.name.len);
+              meas_info_name_str[msg_frm_1->meas_info_lst[z].meas_type.name.len] = '\0';
+
+              // Get the value of the Measurement
+              switch (msg_frm_1->meas_data_lst[j].meas_record_lst[z].value)
+              {
+              case REAL_MEAS_VALUE:
+              {
+                if (strcmp(meas_info_name_str, "DRB.RlcSduDelayDl") == 0)
+                {
+                  printf("DRB.RlcSduDelayDl = %.2f [μs]\n", msg_frm_1->meas_data_lst[j].meas_record_lst[z].real_val);
+                }
+                else if (strcmp(meas_info_name_str, "DRB.UEThpDl") == 0)
+                {
+                  printf("DRB.UEThpDl = %.2f [kbps]\n", msg_frm_1->meas_data_lst[j].meas_record_lst[z].real_val);
+                }
+                else if (strcmp(meas_info_name_str, "DRB.UEThpUl") == 0)
+                {
+                  printf("DRB.UEThpUl = %.2f [kbps]\n", msg_frm_1->meas_data_lst[j].meas_record_lst[z].real_val);
+                }
+                else
+                {
+                  assert(false && "Measurement Name not yet implemented");
+                }
+
+                break;
+              }
+                
+
+              case INTEGER_MEAS_VALUE:
+              {
+                if (strcmp(meas_info_name_str, "RRU.PrbTotDl") == 0)
+                {
+                  printf("RRU.PrbTotDl = %d [PRBs]\n", msg_frm_1->meas_data_lst[j].meas_record_lst[z].int_val);
+                }
+                else if (strcmp(meas_info_name_str, "RRU.PrbTotUl") == 0)
+                {
+                  printf("RRU.PrbTotUl = %d [PRBs]\n", msg_frm_1->meas_data_lst[j].meas_record_lst[z].int_val);
+                }
+                else if (strcmp(meas_info_name_str, "DRB.PdcpSduVolumeDL") == 0)
+                {
+                  printf("DRB.PdcpSduVolumeDL = %d [kb]\n", msg_frm_1->meas_data_lst[j].meas_record_lst[z].int_val);
+                }
+                else if (strcmp(meas_info_name_str, "DRB.PdcpSduVolumeUL") == 0)
+                {
+                  printf("DRB.PdcpSduVolumeUL = %d [kb]\n", msg_frm_1->meas_data_lst[j].meas_record_lst[z].int_val);
+                }
+                else
+                {
+                  assert(false && "Measurement Name not yet implemented");
+                }
+
+                break;
+              }
+              
+              default:
+                assert("Value not recognized");
+              }
+              
+              break;
+            }
+          
+            default:
+              assert(false && "Measurement Type not yet implemented");
+            }
+          }
+          
+
+          if (msg_frm_1->meas_data_lst[j].incomplete_flag && *msg_frm_1->meas_data_lst[j].incomplete_flag == TRUE_ENUM_VALUE)
+            printf("Measurement Record not reliable");
+        }        
       }
     }
-
-    assert(mi->labelInfo_len == 1);
-    if (mi->labelInfo[0].plmn_id != NULL) {
-      const plmn_t* plmn = mi->labelInfo[0].plmn_id;
-      printf(" (PLMN %03d.%0*d)",
-             plmn->mcc, plmn->mnc_digit_len, plmn->mnc);
-    }
-    printf("\n");
+    counter++;
   }
+}
+
+static
+kpm_event_trigger_def_t gen_ev_trig(uint64_t period)
+{
+  kpm_event_trigger_def_t dst = {0};
+
+  dst.type = FORMAT_1_RIC_EVENT_TRIGGER;
+  dst.kpm_ric_event_trigger_format_1.report_period_ms = period;
+
+  return dst;
+}
+
+static
+meas_info_format_1_lst_t gen_meas_info_format_1_lst(const char* action)
+{
+  meas_info_format_1_lst_t dst = {0};
+
+  dst.meas_type.type = NAME_MEAS_TYPE;
+  // ETSI TS 128 552
+  dst.meas_type.name = cp_str_to_ba(action);
+
+  dst.label_info_lst_len = 1;
+  dst.label_info_lst = calloc(1, sizeof(label_info_lst_t));
+  assert(dst.label_info_lst != NULL && "Memory exhausted");
+  dst.label_info_lst[0].noLabel = calloc(1, sizeof(enum_value_e));
+  assert(dst.label_info_lst[0].noLabel != NULL && "Memory exhausted");
+  *dst.label_info_lst[0].noLabel = TRUE_ENUM_VALUE;
+
+  return dst;
+}
+
+static
+kpm_act_def_format_1_t gen_act_def_frmt_1(const char** action)
+{
+  kpm_act_def_format_1_t dst = {0};
+
+  dst.gran_period_ms = 1000;
+
+  // [1, 65535]
+  size_t count = 0;
+  while (action[count] != NULL) {
+    count++;
+  }
+  dst.meas_info_lst_len = count;
+  dst.meas_info_lst = calloc(count, sizeof(meas_info_format_1_lst_t));
+  assert(dst.meas_info_lst != NULL && "Memory exhausted");
+
+  for(size_t i = 0; i < dst.meas_info_lst_len; i++) {
+    dst.meas_info_lst[i] = gen_meas_info_format_1_lst(action[i]);
+  }
+
+  return dst;
+}
+
+static
+kpm_act_def_format_4_t gen_act_def_frmt_4(const char** action)
+{
+  kpm_act_def_format_4_t dst = {0};
+
+  // [1, 32768]
+  dst.matching_cond_lst_len = 1;
+
+  dst.matching_cond_lst = calloc(dst.matching_cond_lst_len, sizeof(matching_condition_format_4_lst_t));
+  assert(dst.matching_cond_lst != NULL && "Memory exhausted");
+
+  // Filter connected UEs by S-NSSAI criteria
+  dst.matching_cond_lst[0].test_info_lst.test_cond_type = S_NSSAI_TEST_COND_TYPE;
+  dst.matching_cond_lst[0].test_info_lst.S_NSSAI = TRUE_TEST_COND_TYPE;
+
+  dst.matching_cond_lst[0].test_info_lst.test_cond = calloc(1, sizeof(test_cond_e));
+  assert(dst.matching_cond_lst[0].test_info_lst.test_cond != NULL && "Memory exhausted");
+  *dst.matching_cond_lst[0].test_info_lst.test_cond = EQUAL_TEST_COND;
+
+  dst.matching_cond_lst[0].test_info_lst.test_cond_value = calloc(1, sizeof(test_cond_value_e));
+  assert(dst.matching_cond_lst[0].test_info_lst.test_cond_value != NULL && "Memory exhausted");
+  *dst.matching_cond_lst[0].test_info_lst.test_cond_value =  INTEGER_TEST_COND_VALUE;
+  dst.matching_cond_lst[0].test_info_lst.int_value = malloc(sizeof(int64_t));
+  assert(dst.matching_cond_lst[0].test_info_lst.int_value != NULL && "Memory exhausted");
+  *dst.matching_cond_lst[0].test_info_lst.int_value = 1;
+
+  printf("[xApp]: Filter UEs by S-NSSAI criteria where SST = %lu\n", *dst.matching_cond_lst[0].test_info_lst.int_value);
+
+  // Action definition Format 1
+  dst.action_def_format_1 = gen_act_def_frmt_1(action);  // 8.2.1.2.1
+
+  return dst;
+}
+
+
+static
+kpm_act_def_t gen_act_def(const char** act)
+{
+  kpm_act_def_t dst = {0};
+
+  dst.type = FORMAT_4_ACTION_DEFINITION;
+  dst.frm_4 = gen_act_def_frmt_4(act);
+  return dst;
 }
 
 int main(int argc, char *argv[])
@@ -94,10 +274,12 @@ int main(int argc, char *argv[])
 
   printf("Connected E2 nodes = %d\n", nodes.len);
 
-  // KPM indication
-  inter_xapp_e i_0 = ms_1000;
-  sm_ans_xapp_t* kpm_handle = NULL;
+  pthread_mutexattr_t attr = {0};
+  int rc = pthread_mutex_init(&mtx, &attr);
+  assert(rc == 0);
 
+  // KPM indication
+  sm_ans_xapp_t* kpm_handle = NULL;
   if(nodes.len > 0){
     kpm_handle = calloc( nodes.len, sizeof(sm_ans_xapp_t) ); 
     assert(kpm_handle  != NULL);
@@ -108,19 +290,31 @@ int main(int argc, char *argv[])
     for (size_t j = 0; j < n->len_rf; j++)
       printf("Registered node %d ran func id = %d \n ", i, n->ack_rf[j].id);
 
-    kpm_handle[i] = report_sm_xapp_api(&nodes.n[i].id, SM_KPM_ID, i_0, sm_cb_kpm);
+    ////////////
+    // START KPM
+    ////////////
+    kpm_sub_data_t kpm_sub = {0};
+    defer({ free_kpm_sub_data(&kpm_sub); });
+
+    // KPM Event Trigger
+    uint64_t period_ms = 1000;
+    kpm_sub.ev_trg_def = gen_ev_trig(period_ms);
+    printf("[xApp]: reporting period = %lu [ms]\n", period_ms);
+
+    // KPM Action Definition
+    kpm_sub.sz_ad = 1;
+    kpm_sub.ad = calloc(1, sizeof(kpm_act_def_t));
+    assert(kpm_sub.ad != NULL && "Memory exhausted");
+    const char *act[] = {"DRB.PdcpSduVolumeDL", "DRB.PdcpSduVolumeUL", "DRB.RlcSduDelayDl", "DRB.UEThpDl", "DRB.UEThpUl", "RRU.PrbTotDl", "RRU.PrbTotUl", NULL}; // 3GPP TS 28.552
+    *kpm_sub.ad = gen_act_def(act);
+
+    const int KPM_ran_function = 2;
+
+    kpm_handle[i] = report_sm_xapp_api(&nodes.n[i].id, KPM_ran_function, &kpm_sub, sm_cb_kpm);
     assert(kpm_handle[i].success == true);
   }
 
-  static bool run_xapp = true;
-  void handle_sig(int signum) {
-    if (signum != SIGINT && signum != SIGTERM)
-      fprintf(stderr, "WARNING: received signal %d, stopping\n", signum);
-    run_xapp = false;
-  }
-  signal(SIGINT, handle_sig);
-  signal(SIGTERM, handle_sig);
-  while(run_xapp) usleep(100000);
+  sleep(100);
 
 
   for(int i = 0; i < nodes.len; ++i){
@@ -138,3 +332,4 @@ int main(int argc, char *argv[])
 
   printf("Test xApp run SUCCESSFULLY\n");
 }
+

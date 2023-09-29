39c1de91cc2501e03468af11b643839334949f80
mir <mikel.irazabal@openairinterface.com>
2023-09-29 09:02:52 +0200

    E1_E2AP_NODE_COMP_INTERFACE_TYPE added

--- src/lib/e2ap/v2_03/e2ap_types/common/e2ap_node_comp_id.c
+++ src/lib/e2ap/v2_03/e2ap_types/common/e2ap_node_comp_id.c
@@ -21,17 +21,29 @@
 
 #include "e2ap_node_comp_id.h"
 #include <assert.h>
+#include <stdbool.h>
+
+static
+bool ensure_type_supported(e2ap_node_comp_interface_type_e type)
+{
+  if(type == NG_E2AP_NODE_COMP_INTERFACE_TYPE 
+      || type == E1_E2AP_NODE_COMP_INTERFACE_TYPE 
+      || type == F1_E2AP_NODE_COMP_INTERFACE_TYPE ){
+    return true; 
+  }
+  return false;
+}
+
 
 void free_e2ap_node_comp_id(e2ap_node_comp_id_t* src)
 {
   assert(src != NULL);
-
-  assert((src->type == NG_E2AP_NODE_COMP_INTERFACE_TYPE || src->type == F1_E2AP_NODE_COMP_INTERFACE_TYPE)  && "Only these two types supported" );
+  assert(ensure_type_supported(src->type));
 
   if(src->type == NG_E2AP_NODE_COMP_INTERFACE_TYPE  ){
     free_byte_array(src->ng_amf_name);
-  } else if (src->type ==  F1_E2AP_NODE_COMP_INTERFACE_TYPE ){
-
+  } else if (src->type == E1_E2AP_NODE_COMP_INTERFACE_TYPE ){
+  } else if (src->type == F1_E2AP_NODE_COMP_INTERFACE_TYPE ){
   } else {
     assert(0 != 0 && "Unknown type");
   }
@@ -41,14 +53,16 @@ void free_e2ap_node_comp_id(e2ap_node_comp_id_t* src)
 e2ap_node_comp_id_t cp_e2ap_node_comp_id(e2ap_node_comp_id_t const* src)
 {
   assert(src != NULL);
-
-  assert((src->type == NG_E2AP_NODE_COMP_INTERFACE_TYPE || src->type == F1_E2AP_NODE_COMP_INTERFACE_TYPE)  && "Only these two types supported" );
+  assert(ensure_type_supported(src->type));
 
   e2ap_node_comp_id_t dst = {.type = src->type }; 
 
   if(src->type == NG_E2AP_NODE_COMP_INTERFACE_TYPE  ){
     dst.ng_amf_name = copy_byte_array(src->ng_amf_name); 
-  } else if (src->type ==  F1_E2AP_NODE_COMP_INTERFACE_TYPE ){
+  } else if (src->type == E1_E2AP_NODE_COMP_INTERFACE_TYPE){
+    assert(src->e1_gnb_cu_up_id < (1UL << 36));
+    dst.e1_gnb_cu_up_id = src->e1_gnb_cu_up_id;
+  } else if (src->type == F1_E2AP_NODE_COMP_INTERFACE_TYPE ){
     assert(src->f1_gnb_du_id < (1UL << 36));
     dst.f1_gnb_du_id = src->f1_gnb_du_id;
   } else {
@@ -66,14 +80,19 @@ bool eq_e2ap_node_comp_id(e2ap_node_comp_id_t const* m0, e2ap_node_comp_id_t con
   if(m0 == NULL || m1 == NULL)
     return false;
 
-  assert((m0->type == NG_E2AP_NODE_COMP_INTERFACE_TYPE || m0->type == F1_E2AP_NODE_COMP_INTERFACE_TYPE)  && "Only these two types supported" );
-  assert((m1->type == NG_E2AP_NODE_COMP_INTERFACE_TYPE || m1->type == F1_E2AP_NODE_COMP_INTERFACE_TYPE)  && "Only these two types supported" );
+  assert(ensure_type_supported(m0->type));
+  assert(ensure_type_supported(m1->type));
 
   if(m0->type != m1->type)
     return false;
 
-  if(m0->type == NG_E2AP_NODE_COMP_INTERFACE_TYPE  ){
+  if(m0->type == NG_E2AP_NODE_COMP_INTERFACE_TYPE){
     if(eq_byte_array(&m0->ng_amf_name, &m1->ng_amf_name) ==false)
+      return false;
+  } else if (m0->type == E1_E2AP_NODE_COMP_INTERFACE_TYPE){
+    assert(m0->e1_gnb_cu_up_id < (1UL << 36));
+    assert(m1->e1_gnb_cu_up_id < (1UL << 36));
+    if(m0->e1_gnb_cu_up_id != m1->e1_gnb_cu_up_id)
       return false;
   } else if (m0->type == F1_E2AP_NODE_COMP_INTERFACE_TYPE ){
     assert(m0->f1_gnb_du_id < (1UL << 36));
